3ce3e7e0dd1cb3bacb08846f26a72ea0d97c53b0
Philipp Sonntag <sonntag.philipp@outlook.de>
2020-02-07 19:14:18 +0100

    Removed old, unused files of the backend api.

--- api/Vivet.Pizza/Models/DishOption.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Vivet.Pizza.Models
-{
-    public class DishOption
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; }
-    }
-}
