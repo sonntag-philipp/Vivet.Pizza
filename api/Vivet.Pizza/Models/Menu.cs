9df51f50a11361f2aa1d64f355cf0336a0a5b569
Philipp Sonntag <sonntag.philipp@outlook.de>
2020-02-07 19:33:28 +0100

    Added specified models.

---
+++ api/Vivet.Pizza/Models/Menu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Vivet.Pizza.Models
+{
+    public class Menu
+    {
+        public int MenuId { get; set; }
+        public string Name { get; set; }
+        public DateTime Version { get; set; }
+    }
+}
---
+++ api/Vivet.Pizza/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Vivet.Pizza.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public string Accessor { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int MenuId { get; set; }
+
+        [ForeignKey("MenuId")]
+        public Menu Menu { get; set; }
+    }
+}
